1ffc072567b1371021571234dab4cc60088564a3
Naohide Sano <umjammer@gmail.com>
2015-03-05 18:35:23 +0800

    off fps display when debug

--- src/main/java/vavi/imageio/bmp/WindowsBitmapImageReader.java
+++ src/main/java/vavi/imageio/bmp/WindowsBitmapImageReader.java
@@ -6,26 +6,21 @@
 
 package vavi.imageio.bmp;
 
-import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.imageio.IIOException;
-import javax.imageio.ImageIO;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import vavi.awt.image.bmp.WindowsBitmapImageSource;
 import vavi.imageio.ImageConverter;
@@ -132,37 +127,6 @@ Debug.println(input);
         java.util.List<ImageTypeSpecifier> l = new ArrayList<ImageTypeSpecifier>();
         l.add(specifier);
         return l.iterator();
-    }
-
-    //----
-
-    /** */
-    public static void main(final String[] args) throws IOException {
-System.err.println(args[0]);
-        ImageReader ir = null;
-        Iterator<ImageReader> irs = ImageIO.getImageReadersByFormatName("BMP");
-        while (irs.hasNext()) {
-            ImageReader tmpIr = irs.next();
-System.err.println("ImageReader: " + tmpIr.getClass().getName());
-            if (tmpIr.getClass().getName().equals(WindowsBitmapImageReader.class.getName())) {
-                ir = tmpIr;
-System.err.println("found ImageReader: " + ir.getClass().getName());
-                break;
-            }
-        }
-        ir.setInput(new FileInputStream(args[0]));
-        final Image image = ir.read(0);
-
-        JFrame frame = new JFrame();
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel() {
-            public void paint(Graphics g) {
-                g.drawImage(image, 0, 0, this);
-            }
-        };
-        frame.getContentPane().add(panel);
-        frame.setVisible(true);
     }
 }
 
